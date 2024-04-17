826e4f25887addd68fb677debb7788d5914c7f5f
jkahne <jkahne@fastmail.com>
2024-04-17 14:38:38 -0400

    don't use enter for typeahead

--- lua/homeless/lazy/lsp.lua
+++ lua/homeless/lazy/lsp.lua
@@ -135,7 +135,7 @@ return {
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        -- ['<CR>'] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete(),
 
         -- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
