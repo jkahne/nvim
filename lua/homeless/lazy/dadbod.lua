604f36cbdb4ed37fda05b434dd242d8d3a930067
jkahne <jkahne@fastmail.com>
2024-04-19 17:37:32 -0400

    checkhealth updates

--- lua/homeless/lazy/dadbod.lua
+++ lua/homeless/lazy/dadbod.lua
@@ -1,6 +1,5 @@
 return {
   "tpope/vim-dadbod",
-  opt = true,
   dependencies = {
     "kristijanhusak/vim-dadbod-ui",
     "kristijanhusak/vim-dadbod-completion",
--- lua/homeless/lazy/init.lua
+++ lua/homeless/lazy/init.lua
@@ -239,7 +239,7 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
-    config = {
+    opts = {
       -- an empty table will work for default config
       --- if you use functions, or whichkey, or lazy to map keys
       --- then please see the API below for options
